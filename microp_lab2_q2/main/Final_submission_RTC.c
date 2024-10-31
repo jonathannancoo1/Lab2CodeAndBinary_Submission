145c2bc3cefc855c037c69e533e66bbe1df5ea00
jonathannancoo1 <jonathan.nancoo@my.uwi.edu>
2024-10-31 14:05:12 -0400

    Update Final_submission_RTC.c

--- microp_lab2_q2/main/Final_submission_RTC.c
+++ microp_lab2_q2/main/Final_submission_RTC.c
@@ -1,77 +1,67 @@
-//816030907 RTC
 
-#include "Final_Submission_RTC.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "esp_sleep.h"
+#include "driver/gpio.h"
+#include "driver/hw_timer.h"
+#include "driver/uart.h"
+#include "driver/i2c.h"
+#include "freertos/semphr.h"
+#include "freertos/FreeRTOSConfig.h"
+#include "ds3231.h"
+#include <i2cdev.h>
+#include <string.h>
 
+#include "NVS_lab2_1.h"  // 
 
-bool init_RTC(){
-
-
-    i2c_config_t pinconfig;
-
-
-    //Installing driver
-    //i2c_driver_install(I2C_NUM_0,I2C_MODE_MASTER);
-    //master mode
-    pinconfig.mode=I2C_MODE_MASTER;
-
-    //disabling pullups
-    pinconfig.sda_pullup_en=GPIO_PULLUP_DISABLE;
-    pinconfig.scl_pullup_en=GPIO_PULLUP_DISABLE;
-
-
-    //Setting SCL as IO 0
-    pinconfig.scl_io_num=GPIO_NUM_0;
-
-    //Setting SDA as IO2
-    pinconfig.sda_io_num=GPIO_NUM_2;
-
-    //wait for 1000 tics
-    pinconfig.clk_stretch_tick = 1000;
-
-
-    i2c_dev_t dev;
-    memset(&dev, 0, sizeof(i2c_dev_t));
-    dev.addr=DS3231_ADDR;
-    dev.cfg=pinconfig;
-    dev.port=I2C_NUM_0;
-
-    esp_err_t status =ds3231_init_desc(&dev,0,GPIO_NUM_2,GPIO_NUM_0);
-    
-    if (status==ESP_OK){
-
-        return true;
-    }
-    
-
-        return false;
-    
-    }
-
-bool write_time_to_RTC(struct tm time){
+struct tm Time = {
+        .tm_year = 116, //since 1900 (2016 - 1900)
+        .tm_mon  = 9,  // 0-based
+        .tm_mday = 9,
+        .tm_hour = 13,
+        .tm_min  = 50,
+        .tm_sec  = 10
+    };
 
 
-i2c_config_t pinconfig;
 
+i2c_config_t pinconfig={
 
 
 //Installing driver
 //i2c_driver_install(I2C_NUM_0,I2C_MODE_MASTER);
  //master mode
-pinconfig.mode=I2C_MODE_MASTER;
+.mode=I2C_MODE_MASTER,
 
 //disabling pullups
-pinconfig.sda_pullup_en=GPIO_PULLUP_DISABLE;
-pinconfig.scl_pullup_en=GPIO_PULLUP_DISABLE;
+.sda_pullup_en=GPIO_PULLUP_DISABLE,
+.scl_pullup_en=GPIO_PULLUP_DISABLE,
 
 
 //Setting SCL as IO 0
-pinconfig.scl_io_num=GPIO_NUM_0;
+.scl_io_num=GPIO_NUM_0,
 
 //Setting SDA as IO2
-pinconfig.sda_io_num=GPIO_NUM_2;
+.sda_io_num=GPIO_NUM_2,
 
 //wait for 1000 tics
-pinconfig.clk_stretch_tick = 1000;
+.clk_stretch_tick = 1000,
+};
+
+static const char *TAG = "example";
+void hello_world_func(){
+
+
+
+    printf("hello world 1 \n");
+
+
 
 
     i2c_dev_t dev;
@@ -80,71 +70,69 @@ pinconfig.clk_stretch_tick = 1000;
     dev.cfg=pinconfig;
     dev.port=I2C_NUM_0;
 
+  if(ds3231_init_desc(&dev,0,GPIO_NUM_2,GPIO_NUM_0)==ESP_OK){
 
-    if (ds3231_set_time(&dev, &time) == ESP_OK)
+
+        printf("work");
+    }
+
+    else
+    {printf("not working");}
+
+
+    if(ds3231_init_desc(&dev,0,GPIO_NUM_2,GPIO_NUM_0)==ESP_OK){
+
+
+        printf("work");
+    }
+
+    else
+    {printf("not working");}
+
+
+    
+         if (ds3231_set_time(&dev, &Time) == ESP_OK)
         {
-            printf("Time has been set\n");
+            printf("Timeset\n");}
+
+            else
+            {printf("Error");}
+
+    printf("hello world 2 \n");
+}
+
+
+void write_to_RTC(){
+
+
+    
+    i2c_dev_t dev2;
+    memset(&dev2, 0, sizeof(i2c_dev_t));
+    dev2.addr=DS3231_ADDR;
+    dev2.cfg=pinconfig;
+    dev2.port=I2C_NUM_0;
+
+    if(ds3231_init_desc(&dev2,0,GPIO_NUM_2,GPIO_NUM_0)==ESP_OK){
+
+
+        printf("work\n");
+    }
+
+    else
+    {printf("not working\n");}
+
+    printf("Here\n");
+
+
+    
+         if (ds3231_set_time(&dev2, &Time) == ESP_OK)
+        {
+            printf("Timeset\n");}
+
+            else
+            {printf("Error\n");}
+
             
-            return true;
-        }
-
-    return false;
-
-    
-  
-}
-
-
-struct tm read_time_from_RTC(){
-
-
-
-    
-i2c_config_t pinconfig;
-
-
-
-//Installing driver
-//i2c_driver_install(I2C_NUM_0,I2C_MODE_MASTER);
- //master mode
-pinconfig.mode=I2C_MODE_MASTER;
-
-//disabling pullups
-pinconfig.sda_pullup_en=GPIO_PULLUP_DISABLE;
-pinconfig.scl_pullup_en=GPIO_PULLUP_DISABLE;
-
-
-//Setting SCL as IO 0
-pinconfig.scl_io_num=GPIO_NUM_0;
-
-//Setting SDA as IO2
-pinconfig.sda_io_num=GPIO_NUM_2;
-
-//wait for 1000 tics
-pinconfig.clk_stretch_tick = 1000;
-   
-   
-    struct tm time;
-    i2c_dev_t dev;
-    memset(&dev, 0, sizeof(i2c_dev_t));
-    dev.addr=DS3231_ADDR;
-    dev.cfg=pinconfig;
-    dev.port=I2C_NUM_0;
-
-if (ds3231_get_time(&dev, &time) != ESP_OK)
-{
-    printf("Year: %d\n", time.tm_year+1900);
-    printf("Month: %d\n", time.tm_mon);
-    printf("Day: %d\n", time.tm_mday);
-    printf("Hour: %d\n", time.tm_hour);
-    printf("Minute: %d\n", time.tm_min);
-    printf("Second: %d\n", time.tm_sec);
-
-    return time;
-}
-printf("Fail \n");
-return time;
+        printf("Here 2\n");
 
 }
-
-
